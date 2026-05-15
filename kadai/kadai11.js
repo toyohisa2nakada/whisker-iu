3ea77e77ae08dc6da47054fdc239f6f00f2adb09
toyohisa <nakada.toyohisa@gmail.com>
2026-05-15 21:02:15 +0900

    Refactor sprite dragging and timing logic

--- kadai/kadai11.js
+++ kadai/kadai11.js
@@ -1,35 +1,30 @@
 const test = async function (t) {
-    
+
     const sprite = t.getSprite('Sprite1') ?? t.getSprite('Cat');
     const chick = t.getSprite('Chick');
     const stage = t.getStage();
-    
-    const x = {sprite:stage.bounds.left,chick:stage.bounds.left};
-    const y = {sprite:sprite.bounds.height,chick:-chick.bounds.height};
 
-    t.dragSprite(sprite.name,x.sprite,y.sprite);
-    t.dragSprite(chick.name,x.sprite,y.chick);
-    
-    console.log(t);
-    
+    t.keyPress('space', 1);
+    await t.runForTime(1000);
+    const x = { sprite: sprite.x, chick: chick.x };
+
     t.greenFlag();
-    for(let i=0;i<5;i+=1){
-        console.log("counter",i);
+    for (let i = 0; i < 5; i += 1) {
+        console.log("counter", i);
         await t.runForTime(500);
-        console.log("sprite x",sprite.x,chick.x);
-        console.log("backuped x",x);
+        console.log("sprite x", sprite.x, chick.x);
+        console.log("backuped x", x);
         t.assert.ok(sprite.x > x.sprite);
         t.assert.ok(chick.x > x.chick);
         x.sprite = sprite.x;
         x.chick = chick.x;
     }
-    t.cancelRun();
-    await t.runForTime(1000);
-    t.keyPress('space',1);
-    await t.runForTime(100);
-    t.assert.ok(sprite.x<x.sprite);
-    t.assert.ok(chick.x<x.chick);
     t.end();
+    await t.runForTime(1000);
+    t.keyPress('space', 1);
+    await t.runForTime(1000);
+    t.assert.ok(sprite.x < x.sprite);
+    t.assert.ok(chick.x < x.chick);
 }
 
 module.exports = [
